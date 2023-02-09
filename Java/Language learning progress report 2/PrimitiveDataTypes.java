2ae2bc95f67485352c98af9c62efac4762447325
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-08 23:28:37 -0600

    Create PrimitiveDataTypes.java

---
+++ Java/Language learning progress report 2/PrimitiveDataTypes.java
@@ -0,0 +1,35 @@
+public class PrimitiveDataTypes {
+  public static void main(String[] args) {
+    // Byte data type
+    byte myByte = 100;
+    System.out.println("Byte: " + myByte);
+
+    // Short data type
+    short myShort = 10000;
+    System.out.println("Short: " + myShort);
+
+    // Integer data type
+    int myInt = 100000;
+    System.out.println("Integer: " + myInt);
+
+    // Long data type
+    long myLong = 10000000000L;
+    System.out.println("Long: " + myLong);
+
+    // Float data type
+    float myFloat = 10.5f;
+    System.out.println("Float: " + myFloat);
+
+    // Double data type
+    double myDouble = 20.5;
+    System.out.println("Double: " + myDouble);
+
+    // Character data type
+    char myChar = 'A';
+    System.out.println("Character: " + myChar);
+
+    // Boolean data type
+    boolean myBoolean = true;
+    System.out.println("Boolean: " + myBoolean);
+  }
+}
