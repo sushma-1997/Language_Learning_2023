cac5612a89399ce625d847ba0d04b58b320c95e3
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-08 23:15:33 -0600

    Create Operators.java

---
+++ Java/Language learning progress report 2/Operators.java
@@ -0,0 +1,104 @@
+//Arithmetic operations:
+
+int a = 10;
+int b = 5;
+
+// Addition
+int sum = a + b;
+System.out.println("Sum: " + sum);
+
+// Subtraction
+int difference = a - b;
+System.out.println("Difference: " + difference);
+
+// Multiplication
+int product = a * b;
+System.out.println("Product: " + product);
+
+// Division
+int quotient = a / b;
+System.out.println("Quotient: " + quotient);
+
+// Modulus
+int remainder = a % b;
+System.out.println("Remainder: " + remainder);
+
+// Comparision operators
+
+int a = 10;
+int b = 5;
+
+// Equal to
+boolean equalTo = a == b;
+System.out.println("Equal to: " + equalTo);
+
+// Not equal to
+boolean notEqualTo = a != b;
+System.out.println("Not equal to: " + notEqualTo);
+
+// Greater than
+boolean greaterThan = a > b;
+System.out.println("Greater than: " + greaterThan);
+
+// Less than
+boolean lessThan = a < b;
+System.out.println("Less than: " + lessThan);
+
+// Greater than or equal to
+boolean greaterThanOrEqualTo = a >= b;
+System.out.println("Greater than or equal to: " + greaterThanOrEqualTo);
+
+// Less than or equal to
+boolean lessThanOrEqualTo = a <= b;
+System.out.println("Less than or equal to: " + lessThanOrEqualTo);
+
+//Logical Operators
+
+boolean a = true;
+boolean b = false;
+
+// Logical AND
+boolean and = a && b;
+System.out.println("AND: " + and);
+
+// Logical OR
+boolean or = a || b;
+System.out.println("OR: " + or);
+
+// Logical NOT
+boolean not = !a;
+System.out.println("NOT: " + not);
+
+//Assignment Operators
+
+int a = 10;
+int b = 5;
+
+// Simple assignment
+a = b;
+System.out.println("a = " + a);
+
+// Add and assign
+a += b;
+System.out.println("a = " + a);
+
+// Subtract and assign
+a -= b;
+System.out.println("a = " + a);
+
+// Multiply and assign
+a *= b;
+System.out.println("a = " + a);
+
+// Divide and assign
+a /= b;
+System.out.println("a = " + a);
+
+// Modulus and assign
+a %= b;
+System.out.println("a = " + a);
+
+
+
+
+
