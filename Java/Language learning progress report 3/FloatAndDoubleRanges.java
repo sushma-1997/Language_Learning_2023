0358341880042073ea8104621243b1e969461528
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-15 23:56:44 -0600

    Update FloatAndDoubleRanges.java

--- Java/Language learning progress report 3/FloatAndDoubleRanges.java
+++ Java/Language learning progress report 3/FloatAndDoubleRanges.java
@@ -1,11 +1,9 @@
 
 System.out.println("Floating value Range (" + Float.MIN_VALUE + " to " + Float.MAX_VALUE + ")");
 
-
-//Floating value Range (1.4E-45 to 3.4028235E38)
+//Output: Floating value Range (1.4E-45 to 3.4028235E38)
 
 
 System.out.println("Floating value Range (" + Double.MIN_VALUE + " to " + Double.MAX_VALUE + ")");
 
-
-//Floating value Range (4.9E-324 to 1.7976931348623157E308)
+//Output: Floating value Range (4.9E-324 to 1.7976931348623157E308)
