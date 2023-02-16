86452faf1778fa4cc620d7bf93f4ae1bee855a81
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-15 23:18:17 -0600

    Create StringExamples.java

---
+++ Java/Language learning progress report 3/StringExamples.java
@@ -0,0 +1,6 @@
+String message = "Hello, world!";
+int length = message.length(); // returns 13
+String upper = message.toUpperCase(); // returns "HELLO, WORLD!"
+String lower = message.toLowerCase(); // returns "hello, world!"
+boolean startsWithHello = message.startsWith("Hello"); // returns true
+boolean endsWithWorld = message.endsWith("world!"); // returns true
