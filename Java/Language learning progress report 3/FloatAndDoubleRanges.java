38b1c0ae2b5d2bed590aaf130a034d1e6314266d
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-15 22:54:37 -0600

    Create FloatAndDoubleRanges.java

---
+++ Java/Language learning progress report 3/FloatAndDoubleRanges.java
@@ -0,0 +1,11 @@
+
+System.out.println("Floating value Range (" + Float.MIN_VALUE + " to " + Float.MAX_VALUE + ")");
+
+
+//Floating value Range (1.4E-45 to 3.4028235E38)
+
+
+System.out.println("Floating value Range (" + Double.MIN_VALUE + " to " + Double.MAX_VALUE + ")");
+
+
+//Floating value Range (4.9E-324 to 1.7976931348623157E308)
