f78c9ba2a283384d322f55f704f1b107fcea9b40
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-22 19:19:31 -0600

    Create keywordsAndexpressions.java

---
+++ Java/Language learning progress report 4/keywordsAndexpressions.java
@@ -0,0 +1,13 @@
+public class Example {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+    
+    if (c > 15) {
+      System.out.println("The sum of a and b is greater than 15.");
+    } else {
+      System.out.println("The sum of a and b is less than or equal to 15.");
+    }
+  }
+}
