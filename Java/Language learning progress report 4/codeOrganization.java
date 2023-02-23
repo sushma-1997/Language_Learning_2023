17ca2abe42fbff3122def4e835a7428ccdfa6c68
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-22 19:23:23 -0600

    Create codeOrganization.java

---
+++ Java/Language learning progress report 4/codeOrganization.java
@@ -0,0 +1,12 @@
+public class Example {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        
+        if (num1 < num2) {
+            System.out.println("num1 is less than num2");
+        } else {
+            System.out.println("num1 is greater than or equal to num2");
+        }
+    }
+}
