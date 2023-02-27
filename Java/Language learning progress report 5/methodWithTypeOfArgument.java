0b86add2ced8b5bff41285fd15e3d5294d942c74
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-26 21:26:33 -0600

    Create methodWithTypeOfArgument.java

---
+++ Java/Language learning progress report 5/methodWithTypeOfArgument.java
@@ -0,0 +1,5 @@
+Calculator calculator = new Calculator();
+
+int result1 = calculator.add(2, 3); // calls the first add method
+int result2 = calculator.add(2, 3, 4); // calls the second add method
+double result3 = calculator.add(2.5, 3.7); // calls the third add method
