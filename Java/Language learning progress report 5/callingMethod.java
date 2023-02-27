9333f8fe194a3a7b08e37df9de053793ec0e2ae6
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-26 21:08:39 -0600

    Update callingMethod.java

--- Java/Language learning progress report 5/callingMethod.java
+++ Java/Language learning progress report 5/callingMethod.java
@@ -1,2 +1,5 @@
 int result = add(3, 5);
 System.out.println(result); // Output: 8
+
+/*The int result = add(3, 5); line calls the add method with the values 3 and 5, and assigns the returned value (8) to the result variable.
+The System.out.println(result); line prints the value of the result variable to the console.*/
