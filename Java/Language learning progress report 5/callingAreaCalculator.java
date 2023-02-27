7669bf59ed48eeeb0ccb036c0f29fd0f06d1bf1c
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-26 21:38:04 -0600

    Create callingAreaCalculator.java

---
+++ Java/Language learning progress report 5/callingAreaCalculator.java
@@ -0,0 +1,3 @@
+double circleArea = AreaCalculator.calculateArea(5.0);
+double rectangleArea = AreaCalculator.calculateArea(10.0, 5.0);
+double triangleArea = AreaCalculator.calculateArea(4.0, 3.0, "triangle");
