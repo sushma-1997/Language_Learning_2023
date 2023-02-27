637d98a83b3c411e16c2728a8b205882a14141d5
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-02-26 21:23:49 -0600

    Create methodOverloading.java

---
+++ Java/Language learning progress report 5/methodOverloading.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+}
