a76df45de3fb540f0d2d103ec4137f4e494f3750
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-03-11 23:52:13 -0600

    Create switchExample.java

---
+++ Java/Language learning progress report 6/switchExample.java
@@ -0,0 +1,30 @@
+int dayOfWeek = 3;
+String dayName;
+
+switch (dayOfWeek) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday";
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day";
+}
+
+System.out.println("Today is " + dayName);
