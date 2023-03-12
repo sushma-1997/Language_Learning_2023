f21459def74e6353a3bd3cda2a2b51bd4fbeee1d
sushma-1997 <98074957+sushma-1997@users.noreply.github.com>
2023-03-11 23:49:27 -0600

    Create switchSyntax.java

---
+++ Java/Language learning progress report 6/switchSyntax.java
@@ -0,0 +1,11 @@
+switch (expression) {
+    case value1:
+        // code to execute if expression equals value1
+        break;
+    case value2:
+        // code to execute if expression equals value2
+        break;
+    // more cases can be added here
+    default:
+        // code to execute if expression does not equal any of the specified values
+}
